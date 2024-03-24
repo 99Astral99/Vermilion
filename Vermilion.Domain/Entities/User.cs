9c32cb7b5c7e7da25b5f66dc39735304d3c9935c
Viktor <strik4462@gmail.com>
2024-03-24 21:40:17 +0300

    feat: implemented iauditable interface to user entity

--- Vermilion.Domain/Entities/User.cs
+++ Vermilion.Domain/Entities/User.cs
@@ -1,17 +1,17 @@
 ﻿using FluentResults;
 using Vermilion.Domain.Common;
+using Vermilion.Domain.Interfaces;
 using Vermilion.Domain.ValueObjects.Identifiers;
 
 namespace Vermilion.Domain.Entities
 {
-    public class User : Entity<UserId>
+    public class User : Entity<UserId>, IAuditable
     {
-        private User(UserId id, string firstName, string lastName, string email, DateTime dateRegistered, string phone) : base(id)
+        private User(UserId id, string firstName, string lastName, string email, string phone) : base(id)
         {
             FirstName = firstName;
             LastName = lastName;
             Email = email;
-            DateRegistered = dateRegistered;
             Phone = phone;
         }
 
@@ -19,16 +19,16 @@ namespace Vermilion.Domain.Entities
         public string? LastName { get; private set; }
         public string Email { get; private set; }
         public string? Phone { get; private set; }
-        //сделать datetime iauditable
-        public DateTime DateRegistered { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
 
         private readonly List<Review> _reviews = new();
         public IReadOnlyList<Review>? Reviews => _reviews?.ToList();
 
-        public static Result<User> Create(string firstName, string lastName, string email, DateTime dateRegistered, string phone)
+        public static Result<User> Create(string firstName, string lastName, string email, string phone)
         {
             var id = UserId.CreateNew();
-            var user = new User(id, firstName, lastName, email, dateRegistered, phone);
+            var user = new User(id, firstName, lastName, email, phone);
 
             return Result.Ok(user);
         }
