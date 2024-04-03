b0f6b0ff31c3f7730780c2d26572663a7b60d44d
Viktor <strik4462@gmail.com>
2024-04-03 18:20:08 +0300

    feat: added tests for application layer & added tests for category entity

---
+++ Vermilion.ApplicationTests/Common/ApplicationFixture..cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Vermilion.Application;
+using Vermilion.Application.Interfaces;
+using Vermilion.Domain.Repositories;
+using Vermilion.Infrastructure;
+
+namespace Vermilion.ApplicationTests.Common
+{
+    public class ApplicationFixture
+    {
+        public ApplicationFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped(typeof(IRepositoryReadOnly<>), typeof(EfRepository<>));
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            serviceCollection.AddDbContext<VermilionDbContext>(
+                opt => opt.UseInMemoryDatabase("TestDb"));
+            serviceCollection.AddScoped<IVermilionDbContext>(provider =>
+                provider.GetService<VermilionDbContext>()!);
+
+            serviceCollection.AddApplication();
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+            _serviceProvider.GetRequiredService<VermilionDbContext>();
+        }
+
+        private readonly ServiceProvider _serviceProvider;
+        public IMediator Mediator => _serviceProvider.GetRequiredService<IMediator>();
+    }
+}
---
+++ Vermilion.ApplicationTests/Common/ClassFixture.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Vermilion.ApplicationTests.Common
+{
+    public abstract class ClassFixture : IClassFixture<ApplicationFixture>
+    {
+        protected readonly IMediator Mediator;
+        protected readonly Guid NotPossibleId;
+        protected readonly Guid UserId;
+        protected readonly string SomeString;
+
+        protected ClassFixture(ApplicationFixture applicationFixture)
+        {
+            Mediator = applicationFixture.Mediator;
+            NotPossibleId = new Guid("3CF3217F-320D-422F-AB2B-41D32B72C656");
+            SomeString = "FA6630D9-05C4-4E79-A649-327AD3819890";
+
+            //on future
+            //UserId = new Guid("{7E0F4D7A-4A70-49F0-95F0-8E70C07447FD}");
+        }
+    }
+}
