867cfcd11194d7d6882dd7e8b02234d81ed054a5
Viktor <strik4462@gmail.com>
2024-05-03 21:13:56 +0300

    added command and controller for Cuisines entity

---
+++ Vermilion.Contracts/Cuisines/Commands/CreateCuisineCommand.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+using Vermilion.Contracts.Responses.Cuisines;
+
+namespace Vermilion.Contracts.Cuisines.Commands
+{
+    public sealed record CreateCuisineCommand
+        ([Required] string Name) : IRequest<Result<ResponseCuisine>>;
+}
